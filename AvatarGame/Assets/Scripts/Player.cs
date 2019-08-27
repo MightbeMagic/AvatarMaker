d81b57e65c89464f9a2b2bac7e28632eb4db635b
Brendan <btcleveland@gmail.com>
2019-08-27 16:36:33 -0600

    Compressed clothing types into dictionaries and cleaned up everything

--- AvatarGame/Assets/Scripts/Player.cs
+++ AvatarGame/Assets/Scripts/Player.cs
@@ -33,14 +33,15 @@ public class Player : MonoBehaviour
 
 	[SerializeField] int m_ranking = 1000;
 
-	public int Ranking { get { return m_ranking; } set { m_ranking -= value; if (m_ranking <= 1) m_ranking = 1; INV_Ranking.text = m_ranking.ToString(); } }
+	public int Ranking { get { return m_ranking; }
+		set
+		{
+			m_ranking = Mathf.Min(value, 1);
+			INV_Ranking.text = m_ranking.ToString();
+		}
+	}
+	public Dictionary<eTypes.ePartType, INV_Clothing> m_equipped = new Dictionary<eTypes.ePartType, INV_Clothing>();
 
-	[SerializeField] public INV_Clothing m_hairEquipted;
-	[SerializeField] public INV_Clothing m_faceEquipted;
-	[SerializeField] public INV_Clothing m_topsEquipted;
-	[SerializeField] public INV_Clothing m_legsEquipted;
-	[SerializeField] public INV_Clothing m_feetEquipted;
-	[SerializeField] public INV_Clothing m_miscEquipted;
 	[SerializeField] public SYS_Wardrobe m_wardrobe;
 
 	[SerializeField] public TextMeshProUGUI INV_Ranking;
@@ -62,55 +63,45 @@ public class Player : MonoBehaviour
 		m_gemsStash.text = gems_currency.ToString();
 		m_heartsStash.text = hearts_currency.ToString();
 	}
-
 	public void UpdateAttributes()
 	{
-		likesScore = (m_hairEquipted != null ? (int)m_hairEquipted.m_like : 0) + (m_faceEquipted != null ? (int)m_faceEquipted.m_like : 0)
-					+ (m_topsEquipted != null ? (int)m_topsEquipted.m_like : 0) + (m_legsEquipted != null ? (int)m_legsEquipted.m_like : 0)
-					+ (m_feetEquipted != null ? (int)m_feetEquipted.m_like : 0) + (m_miscEquipted != null ? (int)m_miscEquipted.m_like : 0);
+		ClearAttributes();
+		
+		foreach(var item in m_equipped.Values)
+		{
+			UpdateAttribute(item);
+		}
 
-		starsScore = (m_hairEquipted != null ? (int)m_hairEquipted.m_star : 0) + (m_faceEquipted != null ? (int)m_faceEquipted.m_star : 0)
-			+ (m_topsEquipted != null ? (int)m_topsEquipted.m_star : 0) + (m_legsEquipted != null ? (int)m_legsEquipted.m_star : 0)
-			+ (m_feetEquipted != null ? (int)m_feetEquipted.m_star : 0) + (m_miscEquipted != null ? (int)m_miscEquipted.m_star : 0);
-
-		gemsScore = (m_hairEquipted != null ? (int)m_hairEquipted.m_gem : 0) + (m_faceEquipted != null ? (int)m_faceEquipted.m_gem : 0)
-			+ (m_topsEquipted != null ? (int)m_topsEquipted.m_gem : 0) + (m_legsEquipted != null ? (int)m_legsEquipted.m_gem : 0)
-			+ (m_feetEquipted != null ? (int)m_feetEquipted.m_gem : 0) + (m_miscEquipted != null ? (int)m_miscEquipted.m_gem : 0);
-
-		heartsScore = (m_hairEquipted != null ? (int)m_hairEquipted.m_heart : 0) + (m_faceEquipted != null ? (int)m_faceEquipted.m_heart : 0)
-			+ (m_topsEquipted != null ? (int)m_topsEquipted.m_heart : 0) + (m_legsEquipted != null ? (int)m_legsEquipted.m_heart : 0)
-			+ (m_feetEquipted != null ? (int)m_feetEquipted.m_heart : 0) + (m_miscEquipted != null ? (int)m_miscEquipted.m_heart : 0);
-
-		if (likesScore > 15) CURR_Likes.SetText("SS");
-		else if (likesScore > 12) CURR_Likes.SetText("S");
-		else if (likesScore > 9) CURR_Likes.SetText("A");
-		else if (likesScore > 6) CURR_Likes.SetText("B");
-		else if (likesScore > 3) CURR_Likes.SetText("C");
-		else if (likesScore > 0) CURR_Likes.SetText("D");
-		else CURR_Likes.SetText("F");
-
-		if (heartsScore > 15) CURR_Hearts.SetText("SS");
-		else if (heartsScore > 12) CURR_Hearts.SetText("S");
-		else if (heartsScore > 9) CURR_Hearts.SetText("A");
-		else if (heartsScore > 6) CURR_Hearts.SetText("B");
-		else if (heartsScore > 3) CURR_Hearts.SetText("C");
-		else if (heartsScore > 0) CURR_Hearts.SetText("D");
-		else CURR_Hearts.SetText("F");
-
-		if (starsScore > 15) CURR_Stars.SetText("SS");
-		else if (starsScore > 12) CURR_Stars.SetText("S");
-		else if (starsScore > 9) CURR_Stars.SetText("A");
-		else if (starsScore > 6) CURR_Stars.SetText("B");
-		else if (starsScore > 3) CURR_Stars.SetText("C");
-		else if (starsScore > 0) CURR_Stars.SetText("D");
-		else CURR_Stars.SetText("F");
-
-		if (gemsScore > 15) CURR_Likes.SetText("SS");
-		else if (gemsScore > 12) CURR_Gems.SetText("S");
-		else if (gemsScore > 9) CURR_Gems.SetText("A");
-		else if (gemsScore > 6) CURR_Gems.SetText("B");
-		else if (gemsScore > 3) CURR_Gems.SetText("C");
-		else if (gemsScore > 0) CURR_Gems.SetText("D");
-		else CURR_Gems.SetText("F");
+		UpdateAttributeText(CURR_Likes, likesScore);
+		UpdateAttributeText(CURR_Hearts, heartsScore);
+		UpdateAttributeText(CURR_Stars, starsScore);
+		UpdateAttributeText(CURR_Gems, gemsScore);
+	}
+	private void UpdateAttributeText(TextMeshProUGUI text, int value)
+	{
+		if (value > 15) text.SetText("SS");
+		else if (value > 12) text.SetText("S");
+		else if (value > 9) text.SetText("A");
+		else if (value > 6) text.SetText("B");
+		else if (value > 3) text.SetText("C");
+		else if (value > 0) text.SetText("D");
+		else text.SetText("F");
+	}
+	private void ClearAttributes()
+	{
+		likesScore = 0;
+		starsScore = 0;
+		gemsScore = 0;
+		heartsScore = 0;
+	}
+	private void UpdateAttribute(INV_Clothing item)
+	{
+		if (item != null)
+		{
+			likesScore += (int)item.m_like;
+			starsScore += (int)item.m_star;
+			gemsScore += (int)item.m_gem;
+			heartsScore += (int)item.m_heart;
+		}
 	}
 }
--- AvatarGame/Assets/Scripts/SYS_Wardrobe.cs
+++ AvatarGame/Assets/Scripts/SYS_Wardrobe.cs
@@ -8,46 +8,35 @@ public class SYS_Wardrobe : MonoBehaviour
 
 	[SerializeField] int m_currentItemCount = 0;
 	[SerializeField] int m_totalItemCount;
-	[SerializeField] Sprite m_blank;
-	[SerializeField] Sprite m_defaultHair;
-	[SerializeField] Sprite m_defaultFace;
-	[SerializeField] Sprite m_defaultTops;
-	[SerializeField] Sprite m_defaultLegs;
+	[SerializeField] private TypeSpriteDictionary m_defaultSprites;
+	[SerializeField] private TypeImageDictionary m_rawImages;
 
 	public List<INV_Clothing> m_wardrobe;
 
-	//[SerializeField] RawImage m_hairBack;
-	[SerializeField] RawImage m_base;
-	[SerializeField] RawImage m_face;
-	[SerializeField] RawImage m_hair;
-	[SerializeField] RawImage m_tops;
-	[SerializeField] RawImage m_legs;
-	[SerializeField] RawImage m_feet;
-	[SerializeField] RawImage m_misc;
-
-
-	private void Start()
+	private void Awake()
 	{
 		m_wardrobe = new List<INV_Clothing>();
-		
-		//m_hairBack.texture = m_blank.texture;
-		m_face.texture = m_defaultFace.texture;
-		m_hair.texture = m_defaultHair.texture;
-		m_tops.texture = m_defaultTops.texture;
-		m_legs.texture = m_defaultLegs.texture;
-		m_feet.texture = m_blank.texture;
-		m_misc.texture = m_blank.texture;
+
+		foreach(eTypes.ePartType type in m_defaultSprites.Keys)
+		{
+			m_rawImages[type].texture = m_defaultSprites[type].texture;
+		}
 	}
 
 
 	public void UpdateItems()
 	{
-		m_face.texture = Player.m_instance.m_faceEquipted == null ? m_defaultFace.texture : Player.m_instance.m_faceEquipted.m_sprite.texture;
-		m_hair.texture = Player.m_instance.m_hairEquipted == null ? m_defaultHair.texture : Player.m_instance.m_hairEquipted.m_sprite.texture;
-		m_tops.texture = Player.m_instance.m_topsEquipted == null ? m_defaultTops.texture : Player.m_instance.m_topsEquipted.m_sprite.texture;
-		m_legs.texture = Player.m_instance.m_legsEquipted == null ? m_defaultLegs.texture : Player.m_instance.m_legsEquipted.m_sprite.texture;
-		m_feet.texture = Player.m_instance.m_feetEquipted == null ? m_blank.texture : Player.m_instance.m_feetEquipted.m_sprite.texture;
-		m_misc.texture = Player.m_instance.m_miscEquipted == null ? m_blank.texture : Player.m_instance.m_miscEquipted.m_sprite.texture;
+		foreach (eTypes.ePartType type in m_defaultSprites.Keys)
+		{
+			if(Player.m_instance.m_equipped.ContainsKey(type) && Player.m_instance.m_equipped[type] != null)
+			{
+				m_rawImages[type].texture = Player.m_instance.m_equipped[type].m_sprite.texture;
+			}
+			else
+			{
+				m_rawImages[type].texture = m_defaultSprites[type].texture;
+			}
+		}
 	}
 
 	public void ObtainItem(INV_Clothing item)
